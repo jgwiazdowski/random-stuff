7862df9441321512ba098beeccfd435e27e3cee5
Jakub Gwiazdowski <jakub.gwiazdowski@hotmail.com>
2020-08-25 22:30:12 +0200

    Add new float and boolean fields

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+
 const PORT = process.env.PORT || 4000;
 
 const typeDefs = gql`
@@ -7,28 +8,71 @@ const typeDefs = gql`
         interestingUrls: [String],
         firstName: String
         email: String
-        pets: [String]
+        pets: [String],
+        randomDiceThrow: Int
+        pi: Float
+        isTodayFriday: Boolean
+        randomCoinUntilTrue: [Boolean]
+        e: Float
+        eulersSeries: [Float]
     } 
 `
 
-const data = {
-    greeting: "Hello World",
-    interestingUrls: [
-        'https://github.com',
-        'https://ea.com',
-        'https://medium.com',
-    ],
-    firstName: "John",
-    email: "john@example.com",
-    pets: ["Mittens", "Doggo", "Birb"]
+
+
+function rootValue() {
+    const getRandomDiceThrow = (sides) => Math.ceil(Math.random() * sides);
+    const today = new Date();
+
+    const randomCoinToss = () => {
+        return Math.random() > 0.5;
+    }
+    const getRandomCoinTossesUntilTrue = () => {
+        const result = [];
+        do {
+            result.push(randomCoinToss());
+        } while(!result[result.length - 1])
+        return result;
+    }
+
+    const eulersSeries = () => {
+        const result = [2];
+        for(i = 0; i < 999 ; i++){            
+            result.push(Math.pow(1+(1/result[i]),result[i]))
+        }
+        return result;
+    }
+
+
+
+
+    const data = {
+        greeting: "Hello World",
+        interestingUrls: [
+            'https://github.com',
+            'https://ea.com',
+            'https://medium.com',
+        ],
+        firstName: "John",
+        email: "john@example.com",
+        pets: ["Mittens", "Doggo", "Birb"],
+        randomDiceThrow: getRandomDiceThrow(6),
+        pi: Math.PI,
+        isTodayFriday: today.getDay() === 5,
+        randomCoinUntilTrue: getRandomCoinTossesUntilTrue(),
+        e: Math.E,
+        eulersSeries: eulersSeries()
+    }
+    return data;
 }
-console.log(process.env);
+
+
+
 const server = new ApolloServer({
     typeDefs,
-    rootValue: data,
+    rootValue,
     playground: true,
     introspection: true
-
 });
 
 
@@ -36,8 +80,8 @@ const server = new ApolloServer({
 
 server.listen({
     port: PORT
-}).then( result => {
+}).then(result => {
     console.log('Server is running at ' + result.url);
-}).catch( err => {
+}).catch(err => {
     console.log('Server failed to run!');
 })
